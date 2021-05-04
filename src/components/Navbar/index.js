7e952ad0f8817bd39d2e8332b03effb4fa5f934f
James V <james.vanhoke@yahoo.com>
2021-05-04 02:05:53 -0700

    Navbar creation

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,10 +3,8 @@ import "./style.css";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div>
-        Big Techies
-      </div>
+    <nav class="navbar fixed-top navbar-dark bg-dark">
+      <span class="navbar-brand mb-0 h1">Employee Directory</span>
     </nav>
   );
 }
