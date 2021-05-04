4c95f6f14af50ffb09fd001e7d88ecfb8a6a4b6e
James V <james.vanhoke@yahoo.com>
2021-05-04 02:05:17 -0700

    Table head component

---
+++ src/components/CardHead/index.js
@@ -0,0 +1,13 @@
+export default function CardHead(props) {
+  return (
+      <thead>
+        <tr>
+          <th scope="col">Photo</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Phone #</th>
+          <th scope="col">Date of Birth</th>
+        </tr>
+      </thead>
+  );
+}
