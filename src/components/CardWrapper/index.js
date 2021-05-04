38742025a16bcfa68ae2761c1172fac4f2de0f59
James V <james.vanhoke@yahoo.com>
2021-05-04 02:06:04 -0700

    CardWrapper

---
+++ src/components/CardWrapper/index.js
@@ -0,0 +1,27 @@
+import CardHead from "../CardHead";
+import Card from "../Card";
+
+export default function CardWrapper({ employees }) {
+  return (
+    <>
+      <table className="table">
+        <CardHead />
+        <tbody>
+          {employees.map(({ picture, name, phone, email, dob, id }) => {
+            return (
+              <Card
+                key={id.value}
+                image={picture.thumbnail}
+                firstName={name.first}
+                lastName={name.last}
+                phone={phone}
+                email={email}
+                date={dob.date.slice(0, -14)}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
