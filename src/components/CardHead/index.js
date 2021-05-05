32acbf41288f20a97196b334bfc5eb4ddea6fe4f
James V <james.vanhoke@yahoo.com>
2021-05-05 02:42:07 -0700

    added sort button

--- src/components/CardHead/index.js
+++ src/components/CardHead/index.js
@@ -1,13 +1,15 @@
-export default function CardHead(props) {
+export default function CardHead({ handleNameSort, sort }) {
   return (
-      <thead>
-        <tr>
-          <th scope="col">Photo</th>
-          <th scope="col">Name</th>
-          <th scope="col">Email</th>
-          <th scope="col">Phone #</th>
-          <th scope="col">Date of Birth</th>
-        </tr>
-      </thead>
+    <thead>
+      <tr>
+        <th scope="col">Photo</th>
+        <th scope="col">
+          Name <button onClick={handleNameSort} className="mx-3 btn btn-secondary">sort by {sort}</button>
+        </th>
+        <th scope="col">Email</th>
+        <th scope="col">Phone #</th>
+        <th scope="col">Date of Birth</th>
+      </tr>
+    </thead>
   );
 }
