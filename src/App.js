672325bb91860263fd42b3b103446bd873b42537
James V <james.vanhoke@yahoo.com>
2021-05-05 00:50:46 -0700

    componentDidUpdate for filtering by search

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   state = {
     employees: [],
     userSearch: "",
+    filteredEmployees: [],
   };
 
   // When component loads onto the page we query our user api
@@ -25,12 +26,30 @@ class App extends Component {
     this.setState({ ...this.state, userSearch: e.target.value });
   };
 
+  // when the search for changes we check if the new value is the same as it was previously. if it isn't we filter our employees by name and pass it into a new array in our state
+  componentDidUpdate(prevProps, prevState) {
+    const searchParam = this.state.userSearch.toLocaleLowerCase();
+    if (this.state.userSearch !== prevState.userSearch) {
+      const filtered = this.state.employees.filter((employee) => {
+        return (
+          employee.name.first.toLowerCase().includes(searchParam) ||
+          employee.name.last.toLowerCase().includes(searchParam)
+        );
+      });
+      this.setState({ ...this.setState, filteredEmployees: filtered });
+    }
+  }
+
+  // Conditionally render between filtered employees if it contains data, or our general employee array using short circuiting
   render() {
+    const isFiltered = this.state.filteredEmployees.length;
     return (
       <div>
         <Navbar handleInputChange={this.handleInputChange} />
         <CardWrapper
-          employees={this.state.employees}
+          employees={
+            isFiltered ? this.state.filteredEmployees : this.state.employees
+          }
         />
       </div>
     );
