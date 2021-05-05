4a1f60d80e4f7e57e9940f8d7a5be2260ebe27db
James V <james.vanhoke@yahoo.com>
2021-05-04 23:40:38 -0700

    added search bar for filtering through employees

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,9 +3,19 @@ import "./style.css";
 
 function Navbar() {
   return (
-    <nav class="navbar fixed-top navbar-dark bg-dark">
-      <span class="navbar-brand mb-0 h1">Employee Directory</span>
-    </nav>
+    <>
+      <nav class="navbar fixed-top navbar-dark bg-dark justify-content-between">
+        <span class="navbar-brand">Navbar</span>
+        <form class="form-inline">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+        </form>
+      </nav>
+    </>
   );
 }
 
